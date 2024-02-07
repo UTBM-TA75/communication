1f89bbef747a1d186df7319882b2d5dfa24ed1c6
rlesage71 <97740871+rlesage71@users.noreply.github.com>
2024-02-07 13:31:22 +0100

    feat: integration composant Samy

--- src/app/pages/chat/chat/chat.component.ts
+++ src/app/pages/chat/chat/chat.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import {ChatFeedComponent} from "../chat-feed/chat-feed.component";
 import {ProfilePreviewComponent} from "../../../shared/components/profile/profile-preview/profile-preview.component";
 import {MessageInputComponent} from "../../../shared/components/inputs/message-input/message-input.component";
+import {
+  ConversationWidgetContainerComponent
+} from "../../dashboard/components/widgets/chat/conversation-widget-container/conversation-widget-container.component";
 
 @Component({
   selector: 'app-chat',
@@ -9,7 +12,8 @@ import {MessageInputComponent} from "../../../shared/components/inputs/message-i
   imports: [
     ChatFeedComponent,
     ProfilePreviewComponent,
-    MessageInputComponent
+    MessageInputComponent,
+    ConversationWidgetContainerComponent
   ],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss'
