6f3f47830452f877cf108d7aac250a547bdf60df
Samy DJEMILI <SamyDjemiliSD@gmail.com>
2024-05-02 15:47:41 +0200

    feat: ordering news to display the most recent one first

--- src/app/pages/dashboard/components/widgets/news/news-widget/news-widget.component.ts
+++ src/app/pages/dashboard/components/widgets/news/news-widget/news-widget.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { News } from '@core/models';
 import { NewsListComponent } from '../news-list/news-list.component';
 
@@ -8,6 +8,13 @@ import { NewsListComponent } from '../news-list/news-list.component';
   imports: [NewsListComponent],
   templateUrl: './news-widget.component.html',
 })
-export class NewsWidgetComponent {
+export class NewsWidgetComponent implements OnInit {
   @Input() newsList!: Array<News>;
+
+  ngOnInit(): void {
+    // Tri des actualités pour afficher la plus récente en premier
+    this.newsList.sort(
+      (a, b) => new Date(b.sendDate).getTime() - new Date(a.sendDate).getTime(),
+    );
+  }
 }
