447ed7ecd259f27162cc112745e68b35d560c620
Samy DJEMILI <SamyDjemiliSD@gmail.com>
2024-05-07 11:01:05 +0200

    refactor(NewsCardComponent): adapting it for collective usage

--- src/app/shared/components/news/news-card/news-card.component.ts
+++ src/app/shared/components/news/news-card/news-card.component.ts
@@ -4,13 +4,15 @@ import { MatIconModule } from '@angular/material/icon';
 import { DatePipe } from '@angular/common';
 
 import { News } from '@core/models';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-news-card',
   standalone: true,
-  imports: [MatCardModule, MatIconModule, DatePipe],
+  imports: [MatCardModule, MatIconModule, DatePipe, RouterLink],
   templateUrl: './news-card.component.html',
 })
 export class NewsCardComponent {
   @Input() news!: News;
+  @Input() fullSize?: boolean = false;
 }
